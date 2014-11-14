08e00aaed121bf97134acc112acdf57c959a042e
pierre <pierrerohel@gmail.com>
2014-11-14 18:19:17 -0500

    changed root

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
-  root :to => "users#index"
+  root :to => "quotes#index"
 
   resources :users
 
